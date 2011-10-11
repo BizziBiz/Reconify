e0c6beaafc11cc57fd25f60e7bb44d802b3c977f
Bouncehost Server <bryanlburkholder@gmail.com>
2011-10-11 15:24:43 -0400

    Update Bizzibiz theme

--- themes/bizzibiz/template.php
+++ themes/bizzibiz/template.php
@@ -4,13 +4,42 @@
 	<base href="/themes/bizzibiz/" />
 	<meta charset=utf-8>
 	<title>Results</title>
-	
-	<link rel="stylesheet" href="style.css" type="text/css" media="screen,projection">
+	<link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+	<link rel="stylesheet" href="css/960.css" type="text/css">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="css/print.css" type="text/css" media="print">
+	<link href='http://fonts.googleapis.com/css?family=Volkhov:400italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-<div id="content">
-	<?=theme::getContent()?>
+<div id="content" class="container_12">
+	<div id="header">
+		<div id="header_left" class="grid_6">
+			<div class="logo"><img src="css/images/logo.png"/></div>
+			<div class="subtext">Quick Report</div>
+		</div>
+		<div id="header_right" class="grid_6">
+			<div id="results">
+				<div id="results_url"><?=URL?></div>
+				<div id="results_grade">
+					<div class="label">grade</div>
+					<div class="value"><?=score::getGrade()?></div>
+				</div>
+				<div id="results_score">
+					<div class="label">score</div>
+					<div class="value"><?=score::display()?></div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div class="clear"></div>
+	<div id="body" class="grid_12">
+		<?=theme::display()?>
+	</div>
+	<div id="footer">
+		Copyright © 2011. Bizzibiz. All rights reserved.
+	</div>
 </div>
 </body>
 </html>
